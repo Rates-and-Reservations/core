fcbc4d450eff753b23340e6a59e4be889c7f5df5
Nimisoere Tekena-Lawson <lawson.nimi@yahoo.com>
2025-03-31 17:22:34 +0100

    Plug merchant routes

--- src/modules/merchant/index.ts
+++ src/modules/merchant/index.ts
@@ -1,2 +1,3 @@
-export * as merchantRoutes from "./merchant.routes";
+export * as merchantRoutes from "./merchant/merchant.routes";
 export * as merchantContactRoutes from "./merchantContact/merchantContact.routes";
+export * as merchantUserRoutes from "./merchantUser/merchantUser.routes";
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
-import { merchantRoutes, merchantContactRoutes } from "@/modules/merchant";
+import { merchantRoutes, merchantContactRoutes, merchantUserRoutes } from "@/modules/merchant";
 
 const router = Router();
 
 router.use('/health', (_req, res) => { res.json({ status: 'UP' }) });
+
+// Merchant routes
 router.use('/merchant', merchantRoutes.default);
 router.use('/merchant-contact', merchantContactRoutes.default);
+router.use('/merchant-user', merchantUserRoutes.default);
 
 export default router;
