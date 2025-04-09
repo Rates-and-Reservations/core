a7118484a8ca2630e39e5a1f6dda52e23d23eea3
Nimisoere Tekena-Lawson <lawson.nimi@yahoo.com>
2025-04-09 22:10:03 +0100

    Add inventory module content

--- src/modules/Inventory/resource/resource.controller.ts
+++ src/modules/Inventory/resource/resource.controller.ts
@@ -2,26 +2,55 @@ import * as ResourceService from './resource.service';
 import { Request, Response } from 'express';
 
 export const create = async (req: Request, res: Response) => {
+  /**
+   * To Do:
+   * - Validate the request body
+   * - Check if the resource already exists
+   * - Check if the user is authorized to create a resource
+   */
   const result = await ResourceService.createResource(req.body);
   res.json(result);
 };
 
 export const list = async (_req: Request, res: Response) => {
+  /**
+   * To Do:
+   * - Check if the user is authorized to list resources
+   * - List only resources that the user is authorized to see
+   * - Add pagination, includes and filtering
+   */
   const results = await ResourceService.listResources();
   res.json(results);
 };
 
 export const getById = async (req: Request, res: Response) => {
+  /**
+   * To Do:
+   * - Check if the user is authorized to get the resource
+   * - Check if the resource exists
+   * - Add includes
+   */
   const result = await ResourceService.getResourceById(req.params.id);
   res.json(result);
 };
 
 export const update = async (req: Request, res: Response) => {
+  /**
+   * To Do:
+   * - Validate the request body
+   * - Check if the user is authorized to update the resource
+   * - Check if the resource exists
+   */
   const result = await ResourceService.updateResource(req.params.id, req.body);
   res.json(result);
 };
 
 export const deleteOne = async (req: Request, res: Response) => {
+  /**
+   * To Do:
+   * - Check if the user is authorized to delete the resource
+   * - Check if the resource exists
+   */
   const result = await ResourceService.deleteResource(req.params.id);
   res.json(result);
 };
@@ -29,30 +58,57 @@ export const deleteOne = async (req: Request, res: Response) => {
 
 /** Disable a resource */
 export const disable = async (req: Request, res: Response) => {
-  // TODO: Implement disable
+  /**
+   * To Do:
+   * - Check if the user is authorized to disable the resource
+   * - Check if the resource exists
+   * - Check if the resource is disabled
+   * - Disable the resource
+   */
   res.json({ message: 'disable not implemented' });
 };
 
 /** Enable a resource */
 export const enable = async (req: Request, res: Response) => {
-  // TODO: Implement enable
+  /**
+   * To Do:
+   * - Check if the user is authorized to enable the resource
+   * - Check if the resource exists
+   * - Check if the resource is enabled
+   * - Enable the resource
+   */
   res.json({ message: 'enable not implemented' });
 };
 
 /** Clone a resource */
 export const clone = async (req: Request, res: Response) => {
-  // TODO: Implement clone
+  /**
+   * To Do:
+   * - Check if the user is authorized to clone the resource
+   * - Check if the resource exists
+   * - Clone the resource
+   */
   res.json({ message: 'clone not implemented' });
 };
 
 /** Check availability of a resource */
 export const checkAvailability = async (req: Request, res: Response) => {
-  // TODO: Implement checkAvailability
+  /**
+   * To Do:
+   * - Check if the user is authorized to check availability of the resource
+   * - Check if the resource exists
+   * - Check if the resource is available for the given date range
+   */
   res.json({ message: 'checkAvailability not implemented' });
 };
 
 /** Get rate for a single resource */
 export const getResourceRates = async (req: Request, res: Response) => {
-  // TODO: Implement getResourceRates
+  /**
+   * To Do:
+   * - Check if the user is authorized to get the resource rates
+   * - Check if the resource exists
+   * - Get the rates for the resource
+   */
   res.json({ message: 'getResourceRates not implemented' });
 };
--- src/modules/Inventory/resourceAsset/resourceAsset.controller.ts
+++ src/modules/Inventory/resourceAsset/resourceAsset.controller.ts
@@ -2,26 +2,59 @@ import * as ResourceAssetService from './resourceAsset.service';
 import { Request, Response } from 'express';
 
 export const create = async (req: Request, res: Response) => {
+  /**
+   * To Do:
+   * - Validate the request body
+   * - Check if the user is authorized to create a merchant asset
+   * - Check if the merchant asset exists
+   * - Upload the asset to the cloud storage
+   * - Assign the asset to the resource
+   * - User can also select existing assets - in this case we should not upload the asset to the cloud storage
+   */
   const result = await ResourceAssetService.createResourceAsset(req.body);
   res.json(result);
 };
 
 export const list = async (_req: Request, res: Response) => {
+  /**
+   * To Do:
+   * - Check if the user is authorized to list assets for a resource
+   * - List only resource assets that the user is authorized to see
+   * - Add pagination, includes and filtering
+   */
   const results = await ResourceAssetService.listResourceAssets();
   res.json(results);
 };
 
 export const getById = async (req: Request, res: Response) => {
+  /**
+   * To Do:
+   * - Check if the user is authorized to get the resource asset
+   * - Check if the resource asset exists
+   * - Add includes
+   */
   const result = await ResourceAssetService.getResourceAssetById(req.params.id);
   res.json(result);
 };
 
 export const update = async (req: Request, res: Response) => {
+  /**
+   * To Do:
+   * - Validate the request body
+   * - We can set primary and sort order
+   * - Check if the user is authorized to update the resource asset
+   */
   const result = await ResourceAssetService.updateResourceAsset(req.params.id, req.body);
   res.json(result);
 };
 
 export const deleteOne = async (req: Request, res: Response) => {
+  /**
+   * To Do:
+   * - Check if the user is authorized to delete the resource asset
+   * - Check if the resource asset exists
+   * - Unassign the asset from the resource by deleting the resource asset
+   */
   const result = await ResourceAssetService.deleteResourceAsset(req.params.id);
   res.json(result);
 };
