c93db8db565e741e941c81c3caba41b7f3c79aa8
Nimisoere Tekena-Lawson <lawson.nimi@yahoo.com>
2025-05-07 19:40:26 +0100

    add prisma optimise

--- src/clients/prisma.ts
+++ src/clients/prisma.ts
@@ -1,5 +1,8 @@
 import { PrismaClient } from '@prisma/client';
+import { withOptimize } from "@prisma/extension-optimize";
 
-const db = new PrismaClient();
+const db = new PrismaClient().$extends(
+  withOptimize({ apiKey: process.env.OPTIMIZE_API_KEY as string })
+);
 
 export default db;
